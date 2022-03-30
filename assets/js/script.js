9b1272de941a07f9a475fe5423518b5dec803f72
Nelson Ramírez <nramirez1850@e-camp.cl>
2022-03-30 11:56:06 -0300

    Se maneja evento submit del formulario de contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,28 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+//función ready de JQuery
+$(() => {
+
+    $("#contacto form").on("submit", function(e){
+        e.preventDefault();
+        let nombre = $('#formulario-nombre').val();
+        let asunto = $('#formulario-asunto').val();
+        let mensaje = $('#formulario-text-area').val();
+        let respuesta = '';
+
+        if(nombre == '' || asunto == '' || mensaje == ''){
+            nombre == '' ? respuesta += '* Debe ingresar un nombre \n' : '';
+            asunto == '' ? respuesta += '* Debe ingresar un asunto \n' : '';
+            mensaje == '' ? respuesta += '* Debe ingresar un mensaje \n': '';
+            alert(`Se ha generado un error al enviar el formulario, por favor verifique lo siguiente:\n${respuesta}`);
+        }else {
+            alert(`Gracias por ponerse en contacto con nosotros: ${nombre}\nRecibirá una respuesta pronto.`);
+            $("#contacto form")[0].reset();
+        }
+
+
+    })
+
+})
