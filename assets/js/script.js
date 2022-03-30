07933d77ecf9db41e2970a99b98faa246b8676db
Nelson Ramírez <nramirez1850@e-camp.cl>
2022-03-30 12:49:44 -0300

    Se cambian alert de mensaje submit formulario por un modal de bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,19 +17,30 @@ $(() => {
     //manejar evento submit del formulario de contacto
     $("#contacto form").on("submit", function(e){
         e.preventDefault();
+        //let modal = $('#modal-formulario');
+        var myModal = new bootstrap.Modal(document.getElementById('modal-formulario'), {
+            keyboard: false
+          })
+          
+
         let nombre = $('#formulario-nombre').val();
         let asunto = $('#formulario-asunto').val();
         let mensaje = $('#formulario-text-area').val();
         let respuesta = '';
 
         if(nombre == '' || asunto == '' || mensaje == ''){
-            nombre == '' ? respuesta += '* Debe ingresar un nombre \n' : '';
-            asunto == '' ? respuesta += '* Debe ingresar un asunto \n' : '';
-            mensaje == '' ? respuesta += '* Debe ingresar un mensaje \n': '';
-            alert(`Se ha generado un error al enviar el formulario, por favor verifique lo siguiente:\n${respuesta}`);
+
+            nombre == '' ? respuesta += '<p>* Debe ingresar un nombre</p>' : '';
+            asunto == '' ? respuesta += '<p>* Debe ingresar un asunto</p>'  : '';
+            mensaje == '' ? respuesta += '<p>* Debe ingresar un mensaje</p>'  : '';
+            $("#modal-formulario .modal-body").html(respuesta);
+            myModal.toggle();
+
         }else {
-            alert(`Gracias por ponerse en contacto con nosotros: ${nombre}\nRecibirá una respuesta pronto.`);
             $("#contacto form")[0].reset();
+            respuesta = `<p>Gracias por ponerse en contacto con nosotros <strong>${nombre}</strong>, responderemos a la brevedad.</p>`
+            $("#modal-formulario .modal-body").html(respuesta);
+            myModal.toggle();
         }
 
 
