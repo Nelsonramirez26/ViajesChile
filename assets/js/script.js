a8470a8d9fa1ea7a09686637b13c8075d59f0bbd
Nelson Ramírez <nramirez1850@e-camp.cl>
2022-03-30 11:41:08 -0300

    Se agregan tooltips a íconos de redes sociales

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//CÓDIGO PARA TOOLTIPS
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
